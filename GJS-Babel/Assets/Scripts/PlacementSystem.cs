87bacd0a7d72345938122de469db9652475c0e87
Luzzi <95920754+LucasLEONARDUZZI@users.noreply.github.com>
2023-09-25 17:12:12 +0200

    gridImprove

--- GJS-Babel/Assets/Scripts/PlacementSystem.cs
+++ GJS-Babel/Assets/Scripts/PlacementSystem.cs
@@ -5,15 +5,54 @@ using UnityEngine;
 [ExecuteAlways]
 public class PlacementSystem : MonoBehaviour
 {
-   
-   public GameObject selector, voxIndicator;
-    
+
+    public enum BlockType
+    {
+        empty,
+        city
+    }
+    public BlockType[,,] worldGrid = new BlockType[100, 100, 100];
+    public Vector3 Control;
+    public GameObject selector, voxIndicator;
+    public Vector3 offset;
     public Grid grid;
+
+    private void Start()
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                for (int z = 0; z < 10; z++)
+                {
+                    worldGrid[x, y, z] = BlockType.empty;
+                }
+            }
+        }
+    }
 
     private void Update()
     {
         Vector3 selectorPosition = selector.transform.position;
         Vector3Int gridPosition = grid.WorldToCell(selectorPosition);
-        voxIndicator.transform.position = grid.CellToWorld(gridPosition);
+        Control = new Vector3(gridPosition.x, gridPosition.y, gridPosition.z);
+        voxIndicator.transform.position = grid.CellToWorld(gridPosition)+offset;
+
+        if (gridPosition.x >= 0 && gridPosition.x < worldGrid.GetLength(0) &&
+            gridPosition.y >= 0 && gridPosition.y < worldGrid.GetLength(1) &&
+            gridPosition.z >= 0 && gridPosition.z < worldGrid.GetLength(2))
+        {
+
+            if (worldGrid[gridPosition.x, gridPosition.y, gridPosition.z] == BlockType.empty)
+            {
+                worldGrid[gridPosition.x, gridPosition.y, gridPosition.z] = BlockType.city;
+                AddBloc(grid.CellToWorld(gridPosition) + offset);
+            }
+        }
+    }
+
+    private void AddBloc(Vector3 spawnPosition)
+    {
+        Instantiate(voxIndicator, spawnPosition, Quaternion.identity);
     }
 }
